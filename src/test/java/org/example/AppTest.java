3c92fb7c96d9fd61aec84e1947239438c5532aad
Andreea <andreeaanbrus@yahoo.com>
2020-03-09 09:37:51 +0200

    add external files

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,8 +1,11 @@
 package org.example;
 
-import static org.junit.Assert.assertTrue;
-
+import domain.Student;
+import org.junit.Assert;
 import org.junit.Test;
+import repository.StudentXMLRepo;
+
+import static org.junit.Assert.*;
 
 /**
  * Unit test for simple App.
@@ -16,5 +19,20 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void souldAddStudent() {
+        StudentXMLRepo studentXMLRepo = new StudentXMLRepo("fisiere/studentTest.xml");
+        Student student = new Student("12", "Andreea Anbrus", 931, "aiie2252@scs.ubbcluj.ro");
+        studentXMLRepo.save(student);
+        assertNotNull(studentXMLRepo.findOne("12"));
+    }
+
+    @Test
+    public void shouldNotAddStudent() {
+        StudentXMLRepo studentXMLRepo = new StudentXMLRepo("fisiere/studentTest.xml");
+        Student student = new Student("", "Andreea Anbrus", 931, "aiie2252@scs.ubbcluj.ro");
+        assertNull(studentXMLRepo.findOne(""));
     }
 }
